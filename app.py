c60ddcd49b04744bdcc6615b5f83256864a78040
samvela <samuelverso2708@gmail.com>
2025-03-04 00:27:27 -0600

    Se crea update_post.html

--- app.py
+++ app.py
@@ -74,6 +74,20 @@ def create_data():
     db.session.commit()
     print("Post creado exitosamente.")
 
+# Ruta para actualizar un post
+@app.route('/post/update/<int:id>', methods=['GET', 'POST'])
+def update_post(id):
+    post = Post.query.get(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.category_id = request.form['category_id']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('index'))
+    
+    categories = Category.query.all()
+    return render_template('update_post.html', post=post, categories=categories)
+
 # Ejecutar la función cuando se corra el script
 if __name__ == '__main__':
     with app.app_context():
